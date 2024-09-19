283622a30cef152237fdf2c60b879599dfbad4c4
Ethan <croweleb@mail.uc.edu>
2024-09-19 13:08:36 -0400

    secondary commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args)
     {
+        int intOperandA = 10;
+        int intOperandB = 28
 
+        int intSum = 0;
+        int intProduct = 0;
+        int intDifference = 0;
+        int intQuotient = 0;
+        int intModulo = 0;
+
+        intSum = intOperandA + intOperandB;
+        intProduct = intOperandA * intOperandB;
+        intDifference = intOperandA - intOperandB;
+        intQuotient = intOperandA / intOperandB;
+        intModulo = intOperandA % intOperandB;
     }
 }
