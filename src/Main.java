7f8dddfe18995e4d94504faec6e9e03c3ca44479
Ethan <crowelle550@gmail.com>
2024-09-19 13:21:41 -0400

    secondary commit

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,19 @@ public class Main {
         int intModulo = 0;
 
         intSum = intOperandA + intOperandB;
-        System.out.println("The sum using two ints of " + intOperandA + " and " +intOperandB + " is " + intSum)
+        System.out.println("The sum using two ints of " + intOperandA + " and " +intOperandB + " is " + intSum);
+
         intProduct = intOperandA * intOperandB;
+        System.out.println("The product using two ints of " + intOperandA + " and " +intOperandB + " is " + intProduct);
+
         intDifference = intOperandA - intOperandB;
+        System.out.println("The difference using two ints of " + intOperandA + " and " +intOperandB + " is " + intDifference);
+
         intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient using two ints of " + intOperandA + " and " +intOperandB + " is " + intQuotient);
+
         intModulo = intOperandA % intOperandB;
+        System.out.println("The modulo using two ints of " + intOperandA + " and " +intOperandB + " is " + intModulo);
 
 
     }
